9d51bda2c79ed34ac393d8e323da84baef652ea8
Eric Guerra Ribeiro <eric.guerra.07@gmail.com>
2022-04-02 10:42:42 -0300

    Add theoric reference to frontal avaliation

--- eric.ribeiro_lab2/avaliarFrontal.m
+++ eric.ribeiro_lab2/avaliarFrontal.m
@@ -1,13 +1,17 @@
 inicializarFrontal
 
+x_theory = @(t) xr*(1-exp(-Kx*t));
+
 out = sim("frontal.slx");
 
 hold on
 plot(out.x.time, out.x.signals.values, "-g", LineWidth=2)
-plot([min(out.x.time), max(out.x.time)], [xr, xr], "--y", LineWidth=2)
+plot(out.x.time, x_theory(out.x.time), "--y", LineWidth=2)
+plot([min(out.x.time), max(out.x.time)], [xr, xr], "--b", LineWidth=2)
 title("Posição X - Controle Frontal")
 xlabel("Tempo (s)")
 ylabel("Posição (m)")
+legend("Resultado Simulação", "Curva Teórica", "x_r")
 hold off
 
 print -depsc2 'q1_x.eps'
