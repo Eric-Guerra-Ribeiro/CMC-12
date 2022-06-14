6173c0082eb61fb99597c8fdbd6b42d33ee41a04
Eric Guerra Ribeiro <eric.guerra.07@gmail.com>
2022-06-14 20:09:56 -0300

    Fix solutions

--- eric.ribeiro_lista8/questao5.m
+++ eric.ribeiro_lista8/questao5.m
@@ -5,7 +5,7 @@ Mr = 0.3546;
 v = 1;
 wb = 6;
 xi = sqrt((1 - sqrt(1 - 10^(-Mr/10)))/2);
-wn = wb/sqrt(1-2*xi^2*sqrt(2 - 4*xi^2 + 4*xi^4));
+wn = wb/sqrt(1-2*xi^2 + sqrt(2 - 4*xi^2 + 4*xi^4));
 Kp = wn/(2*xi*v);
 Kpsi = 2*xi*wn;
 end
