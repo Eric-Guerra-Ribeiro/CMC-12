4ab65d1502e24100cb8374b2124eb679776bd2cd
Eric Guerra Ribeiro <eric.guerra.07@gmail.com>
2022-06-28 16:03:55 -0300

    Do question 1

--- eric.ribeiro_lista10/questao1.m
+++ eric.ribeiro_lista10/questao1.m
@@ -5,5 +5,8 @@ function [GM, PM, wcg, wcp] = questao1(mag, fase, w)
 % considerando as frequencias dadas por w (rad/s). GM e PM sao as margens
 % de ganho e de fase, respectivamente, que sao medidas em wcg e wcp,
 % respectivamente.
-
+wcp = interp1(mag, w, 0);
+wcg = interp1(fase, w, -180);
+GM = -interp1(w, mag, wcg);
+PM = 180 + interp1(w, fase, wcp);
 end
